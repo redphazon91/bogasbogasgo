582b7be8e405a4129a3f5f9e25cca89d5a24df50
Red Phazon <137455649+redphazon91@users.noreply.github.com>
2026-02-10 23:54:30 -0300

    feat: autopromocao
    criei uma barra de favoritos para mim se lamber

--- src/main.py
+++ src/main.py
@@ -43,9 +43,36 @@ class SimpleBrowser(QMainWindow):
         nav_bar.addWidget(self.reload_btn)
         nav_bar.addWidget(self.url_bar)
 
+        # 3. Favorites Bar
+        favorites_bar = QHBoxLayout()
+
+        self.favorites = [
+            {
+                "name": "X",
+                "url": r"https://x.com/aied_online",
+            },
+            {
+                "name": "GitHub",
+                "url": r"https://github.com/naoimportaweb",
+            },
+            {
+                "name": "Angadosu (YouTube)",
+                "url": r"https://www.youtube.com/@angado_su/featured",
+            },
+        ]
+
+        for favorite in self.favorites:
+            btn = QPushButton(favorite["name"])
+            btn.clicked.connect(
+                lambda _, url=favorite["url"]: self.browser.setUrl(QUrl(url))
+            )
+            btn.setToolTip(f'Ir para {favorite["name"]}\nem "{favorite["url"]}"')
+            favorites_bar.addWidget(btn)
+
         # 3. Layout Setup
         layout = QVBoxLayout()
         layout.addLayout(nav_bar)
+        layout.addLayout(favorites_bar)
         layout.addWidget(self.browser)
 
         container = QWidget()
